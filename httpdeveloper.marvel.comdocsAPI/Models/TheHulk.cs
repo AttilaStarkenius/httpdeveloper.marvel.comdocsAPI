46a72ccc8f04edefbefc876afbe2e327e3215903
Attila Starkenius <symphtom@hotmail.com>
2020-03-24 23:22:15 +0100

    Add project files.

---
+++ httpdeveloper.marvel.comdocsAPI/Models/TheHulk.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace httpdeveloper.marvel.comdocsAPI.Models
+{
+    public class TheHulk
+    {
+        public string theStoryName { get; set; }
+        public string theCharacterName { get; set; }
+        public int thePictureID { get; set; }
+        public string theMarvelAttributionText { get; set; }
+    }
+}
